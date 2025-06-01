6282207e53d3e7b4f08d57115a5ff8da6c163c5c
ttheek <tharushatheekshana4@gmail.com>
2025-06-01 22:24:34 +0530

    Docs for AdsbdbAPI class methods

--- adsbdb/api.js
+++ adsbdb/api.js
@@ -1,10 +1,24 @@
 import APIRequest from "./request.js";
 import { Aircraft, Airline, Callsign } from "./entities.js";
 
+/**
+ * Main class of the AdsbdbAPI
+ */
 class AdsbdbAPI{
+    /**
+     * Initializes a new instance of the AdsbdbAPI class.
+     */
     constructor() {
         this.apiRequest = new APIRequest();
     }
+
+    /**
+     * Fetches information about an aircraft based on its code.
+     * 
+     * @param {string} code - Mode-S or Registration of the aircraft.
+     * @returns {Promise<Aircraft>} A promise that resolves to an Aircraft object.
+     * @throws {Error} If there is an issue with the API request.
+     */
     async getAircraft(code) {
         try {
             const data = await this.apiRequest.get(`/aircraft/${code}`);
@@ -15,6 +29,13 @@ class AdsbdbAPI{
         }
     }
 
+    /**
+     * Fetches information about an airline based on its IATA or ICAO code.
+     * 
+     * @param {string} code - The IATA (2 characters) or ICAO (3 characters) code of the airline.
+     * @returns {Promise<Airline>} A promise that resolves to an Airline object.
+     * @throws {Error} If the code is invalid or there is an issue with the API request.
+     */
     async getAirline(code) {
         if (!code || typeof code !== "string" || code.length !== 2 && code.length !== 3) {
             throw new Error("Invalid IATA or ICAO code");
@@ -29,6 +50,13 @@ class AdsbdbAPI{
         }
     }
 
+    /**
+     * Fetches information about a flight route based on its callsign.
+     * 
+     * @param {string} callsign - The callsign of the flight.
+     * @returns {Promise<Callsign>} A promise that resolves to a Callsign object.
+     * @throws {Error} If the callsign is invalid or there is an issue with the API request.
+     */
     async getCallsign(callsign) {
         if (!callsign || typeof callsign !== "string") {
             throw new Error("Invalid callsign");
